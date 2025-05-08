1d773a8d854312df823d345e7848959962c445ca
Dulcelene Machado <dudutola@gmail.com>
2025-05-08 15:11:46 +0100

    Added sections#show to displays sections details

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,5 +1,15 @@
 class SectionsController < ApplicationController
+  before_action :set_section, only: [ :show ]
+
   def index
     @sections = Section.all
+  end
+
+  def show; end
+
+  private
+
+  def set_section
+    @section = Section.find(params[:id])
   end
 end
