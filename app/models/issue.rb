f7fa22d547ca75c132bc0746a6b594b6f95205ed
Dulcelene Machado <dudutola@gmail.com>
2025-05-09 16:24:42 +0100

    Added associations to keyword and issue model

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,8 @@
 class Issue < ApplicationRecord
   belongs_to :section
   has_one_attached :image
+  has_many :issue_keywords, dependent: :destroy
+  has_many :keywords, through: :issue_keywords
 
   validates :title, presence: true
 end
--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,3 +1,6 @@
 class Keyword < ApplicationRecord
+  has_many :issue_keywords, dependent: :destroy
+  has_many :issues, through: :issue_keywords
+
   validates :word, presence: true
 end
